9b472cc0d96ee48e1d9eaacd14b06b669108b02d
Nathan Hugon <nthugon@gmail.com>
2018-09-21 17:33:49 -0700

    set up simple structure and routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './App.css';
+
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const Beers = () => (
+  <div>
+    <h2> Beers </h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2> About </h2>
+  </div>
+);
+
+function Header() {
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/beers">Beers</Link></li>
+        <li><Link to="/about">About</Link></li>        
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div> Another awesome app by Nathan Hugon!</div>
+  );
+}
 
 class App extends Component {
   render() {
+
+    const DefaultLayout = ({component: Component, ...rest}) => {
+      return (
+        <Route {...rest} render={matchProps => (
+          <div className="DefaultLayout">
+            <Header />
+            <Component {...matchProps} />
+            <Footer />
+          </div>
+        )} />
+      );
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <DefaultLayout path="/" exact component={Home} />
+        <DefaultLayout path="/beers" exact component={Beers} />
+        <DefaultLayout path="/about" component={About} />
       </div>
     );
   }
