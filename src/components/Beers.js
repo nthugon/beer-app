f969a1fea2e5953403b6f690d5b0415e6d6b77d6
Nathan Hugon <nthugon@gmail.com>
2018-09-28 20:36:32 -0700

    use fetch to get list of beers; style the beers list

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Beers.css';
 
-const Beers = () => (
-  <div>
-    <h2>Beers</h2>
-  </div>
-);
+class Beers extends Component {
+  constructor() {
+    super();
+    this.state = { beers: [] };
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://brj-server.herokuapp.com/api/beers');
+    const json = await response.json();
+    console.log(json);
+    this.setState({ beers: json });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 className="content-heading">Select a Beer to See Reviews</h3>
+        <div>
+          {this.state.beers.map(beer => (
+            <div className="beer-listing" key={beer.name}>
+              <div title="{beer.brewery}" className="detail-line">
+                {beer.brewery} 
+              </div>
+              <div title="{beer.name}" className="title-line">
+                {beer.name} 
+              </div>
+              <div className="detail-line">
+                {beer.style} {beer.abv}<span>{beer.abv ? "% abv" : ""}</span>
+              </div>
+            </div>
+          ))}        
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Beers;
