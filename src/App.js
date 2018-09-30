0dfacfe71e96a476b58dba610f03b3e8908a8e03
Nathan Hugon <nthugon@gmail.com>
2018-09-30 15:40:02 -0700

    add loading and error reporting to fetching of data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import Subheading from './components/Subheading';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Beers from './components/Beers';
 import About from './components/About';
-import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -4,37 +4,90 @@ import './Beers.css';
 class Beers extends Component {
   constructor() {
     super();
-    this.state = { beers: [] };
+    this.state = {
+      error: null,
+      isLoaded: false,
+      beers: [],
+    };
   }
 
   async componentDidMount() {
-    const response = await fetch('https://brj-server.herokuapp.com/api/beers');
-    const json = await response.json();
-    console.log(json);
-    this.setState({ beers: json });
+    try {
+      const response = await fetch('https://brj-server.herokuapp.com/api/beers');
+      const json = await response.json();
+      if (response.ok) {        
+        this.setState({
+          isLoaded: true,
+          beers: json
+        });
+      } else {
+        this.setState({
+          isLoaded: true,
+          error: json.error
+        });
+      }
+    } catch(error) {
+      this.setState({
+        isLoaded: true,
+        error: error.message
+      });
+    }
   }
 
+  // componentDidMount() {
+  //   fetch('https://brj-server.herokuapp.com/api/revie')
+  //     .then(result => result.json())
+  //     .then(
+  //       (result) => {
+  //         if (result.ok) {            
+  //           this.setState({
+  //             isLoaded: true,
+  //             beers: result
+  //           });
+  //         } else {
+  //           this.setState({
+  //             isLoaded: true,
+  //             error: result.error
+  //           });
+  //         }
+  //       },
+  //       (error) => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           error: error.message
+  //         });
+  //       }
+  //     );
+  // }
+
   render() {
-    return (
-      <div>
-        <h3 className="content-heading">Select a Beer to See Reviews</h3>
+    const { error, isLoaded, beers } = this.state;
+    if (error) {
+      return <div className="content-heading error">Error: {error}</div>;
+    } else if (!isLoaded) {
+      return <div className="content-heading">Loading...</div>;
+    } else {
+      return (
         <div>
-          {this.state.beers.map(beer => (
-            <div className="beer-listing" key={beer.name}>
-              <div title="{beer.brewery}" className="detail-line">
-                {beer.brewery} 
+          <h3 className="content-heading">Select a Beer to See Reviews</h3>
+          <div>
+            {beers.map(beer => (
+              <div className="beer-listing" key={beer.name}>
+                <div title="{beer.brewery}" className="detail-line">
+                  {beer.brewery} 
+                </div>
+                <div title="{beer.name}" className="title-line">
+                  {beer.name} 
+                </div>
+                <div className="detail-line">
+                  {beer.style} {beer.abv}<span>{beer.abv ? "% abv" : ""}</span>
+                </div>
               </div>
-              <div title="{beer.name}" className="title-line">
-                {beer.name} 
-              </div>
-              <div className="detail-line">
-                {beer.style} {beer.abv}<span>{beer.abv ? "% abv" : ""}</span>
-              </div>
-            </div>
-          ))}        
+            ))}        
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
